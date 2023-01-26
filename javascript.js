8e76b3166bf9952929d3b54583de1b99b44fba94
Rockfordgun <anton@sunstroke.org>
2023-01-26 04:29:53 +0200

    first commit

---
+++ javascript.js
@@ -0,0 +1,27 @@
+function Calculator() {
+    
+    let answer = document.querySelector ('#answer');
+    let num1 = document.querySelector ('#num1');
+    let num2 = document.querySelector ('#num2');
+
+console.log(num1, num2);
+    
+
+}
+
+
+
+/*
+
+function getSelector () {
+
+}
+
+function getNum2 () {
+
+}
+
+function getCalculation () {
+
+}
+*/

c21a3c3980177afd3e3e9603f9ab3dcc63c5197b
Rockfordgun <anton@sunstroke.org>
2023-01-26 04:34:26 +0200

    added variables

--- javascript.js
+++ javascript.js
@@ -3,8 +3,13 @@ function Calculator() {
     let answer = document.querySelector ('#answer');
     let num1 = document.querySelector ('#num1');
     let num2 = document.querySelector ('#num2');
+    let select = document.querySelector('select');
+    let operator = select.options [select.selectedIndex].value;
+    var theAnswer;
 
-console.log(num1, num2);
+    if (operator == 'add') {
+        
+    }
     
 
 }
